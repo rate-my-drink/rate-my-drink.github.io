768900d87843b3b8b413916f5b9ffd751d9eadc7
wagenrace <nijhof.dns@gmail.com>
2023-08-29 14:11:19 +0200

    empty vue app

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div class="">
+    <h1>Coffee and Tea app</h1>
+    <p>An app for all your coffee and tea rating. Find the best</p>
+  </div>
+</template>
