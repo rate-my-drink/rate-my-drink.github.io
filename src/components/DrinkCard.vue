8eec92039fb6606b08a5a02666762deed771dd10
wagenrace <nijhof.dns@gmail.com>
2023-08-31 23:04:34 +0200

    add coffee cards

---
+++ src/components/DrinkCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps(['product'])
+const product = ref(props.product);
+
+const addToCart = () => {
+    // You can implement your add to cart logic here
+};
+</script>
+<template>
+    <div class="bg-white rounded-lg shadow-lg h-96">
+        <img :src="product.image_url" alt="Product Image" class="rounded-t-lg w-full h-48 object-cover">
+        <div class="p-4">
+            <h2 class="text-xl font-semibold mb-2">{{ product.name }}</h2>
+            <p class="text-gray-700 mb-4">{{ product.description }}</p>
+        </div>
+    </div>
+</template>
+  
+ 
+  
---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+  
+<template>
+    <div class="flex items-center justify-between bg-amber-600 py-6">
+        <button @click="console.log('/')" class="text-2xl font-bold text-black px-4">Home</button>
+        <button @click="console.log('/about')" class="text-2xl font-bold text-black px-4">About</button>
+        <button @click="console.log('/contact')" class="text-2xl font-bold text-black px-4">Contact</button>
+    </div>
+</template>
+  
