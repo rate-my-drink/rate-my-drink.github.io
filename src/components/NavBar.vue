f7ae4b272bd363ff29d415bd822cdacaa9511ce4
wagenrace <nijhof.dns@gmail.com>
2023-09-01 14:03:29 +0200

    prettier navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
 </script>
   
 <template>
-    <div class="flex items-center justify-between bg-amber-600 py-6">
+    <div class="flex items-center justify-around bg-amber-500 py-6">
         <button @click="console.log('/')" class="text-2xl font-bold text-black px-4">Home</button>
         <button @click="console.log('/about')" class="text-2xl font-bold text-black px-4">About</button>
         <button @click="console.log('/contact')" class="text-2xl font-bold text-black px-4">Contact</button>
