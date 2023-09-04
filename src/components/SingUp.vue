ad80ebc1c7607dc748a8f0562d2bb1654f016e2c
wagenrace <nijhof.dns@gmail.com>
2023-09-04 22:47:40 +0200

    add send to create account

--- src/components/SingUp.vue
+++ src/components/SingUp.vue
@@ -1,30 +1,49 @@
 <script setup lang="ts">
+import { supabase } from "../config/supabase"
+import { ref } from 'vue';
+
 const emit = defineEmits(['update:modelValue']);
+const email = ref("")
+const password = ref("")
+const password2 = ref("")
+const displayName = ref("")
 
 const clickBackground = (): void => {
     emit('update:modelValue', false);
 };
+
+async function signup() {
+    if (password.value == password2.value) {
+        console.log("Create account")
+        await supabase.auth.signUp({
+            email: email.value,
+            password: password.value,
+        })
+    }
+}
+
 </script>
 <template>
     <div class="h-screen w-screen fixed bg-transparent-black-50" @click="clickBackground">
 
         <div class="bg-grey-lighter min-h-screen flex flex-col">
             <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2" v-on:click.stop>
-                <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                <div class="bg-slate-200 px-6 py-8 rounded shadow-md text-black w-full">
                     <h1 class="mb-8 text-3xl text-center">Sign up</h1>
                     <input type="text" class="block border border-grey-light w-full p-3 rounded mb-4" name="fullname"
-                        placeholder="Name to Display" />
+                        v-model="displayName" placeholder="Name to Display" />
 
                     <input type="text" class="block border border-grey-light w-full p-3 rounded mb-4" name="email"
-                        placeholder="Email" />
+                        v-model="email" placeholder="Email" />
 
                     <input type="password" class="block border border-grey-light w-full p-3 rounded mb-4" name="password"
-                        placeholder="Password" />
+                        v-model="password" placeholder="Password" />
                     <input type="password" class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="confirm_password" placeholder="Confirm Password" />
+                        name="confirm_password" v-model="password2" placeholder="Confirm Password" />
 
                     <button type="submit"
-                        class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1">Create
+                        class="w-full text-center py-3 rounded-xl bg-green text-black bg-amber-700 hover:bg-amber-500 focus:outline-none my-1"
+                        @click="signup">Create
                         Account</button>
 
                     <div class="text-center text-sm text-grey-dark mt-4">
