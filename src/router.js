b733ad195ac563bfc4f1bc33016fe8054c661097
wagenrace <nijhof.dns@gmail.com>
2023-09-14 10:13:20 +0200

    move home to its own view

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+export const path_home = import.meta.env.BASE_URL;
+export const path_drinks = import.meta.env.BASE_URL + "drinks";
+export const path_contact = import.meta.env.BASE_URL + "contact";
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: path_home,
+      component: () => import("@/views/Home.vue"),
+      name: "Home",
+    },
+    // {
+    //   path: path_drinks,
+    //   component: () => import("@/views/Drinks.vue"),
+    //   name: "Drinks",
+    // },
+    // {
+    //   path: path_contact,
+    //   component: () => import("@/views/Contact.vue"),
+    //   name: "Contact",
+    // },
+  ],
+});
