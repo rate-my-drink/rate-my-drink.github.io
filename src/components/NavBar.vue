e9af79ea723fae4155be0dfba48e55c9396f4e0a
wagenrace <nijhof.dns@gmail.com>
2023-09-18 15:23:43 +0200

    add routing buttons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,9 +12,9 @@ const showSignUp = ref(false)
 <template>
     <SingUp v-show="showSignUp" v-model="showSignUp" />
     <div class="flex items-center justify-around bg-amber-500 py-6">
-        <button @click="console.log('/')" class="text-2xl font-bold text-black px-4">Home</button>
-        <button @click="console.log('/about')" class="text-2xl font-bold text-black px-4">About</button>
-        <button @click="console.log('/contact')" class="text-2xl font-bold text-black px-4">Contact</button>
+        <router-link class="text-2xl font-bold text-black px-4" to="/">Home</router-link>
+        <router-link class="text-2xl font-bold text-black px-4" to="/">About</router-link>
+        <router-link class="text-2xl font-bold text-black px-4" to="/">Contact</router-link>
         <button v-if="userName === null" @click="showSignUp = !showSignUp" class="text-2xl font-bold text-black px-4">Sign
             up</button>
         <button v-if="userName" @click="logout()" class="text-2xl font-bold text-black px-4">Sign out</button>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 export const path_home = import.meta.env.BASE_URL;
-export const path_drinks = import.meta.env.BASE_URL + "drinks/:id";
+export const path_drinks = import.meta.env.BASE_URL + "drink/:id";
 export const path_contact = import.meta.env.BASE_URL + "contact";
 
 export default createRouter({
@@ -15,7 +15,7 @@ export default createRouter({
     {
       path: path_drinks,
       component: () => import("@/views/Drink.vue"),
-      name: "Drinks",
+      name: "Drink",
     },
     // {
     //   path: path_contact,
