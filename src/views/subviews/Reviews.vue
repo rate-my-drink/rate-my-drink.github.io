31c4f25bc7868c00c52bad33275113c727f21c5b
wagenrace <nijhof.dns@gmail.com>
2023-10-05 21:48:42 +0200

    show all reviews

--- src/views/subviews/Reviews.vue
+++ src/views/subviews/Reviews.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, toRefs, ref } from "vue";
+import { toRefs, ref } from "vue";
 import { supabase } from "../../config/supabase.ts"
 import { inject } from 'vue'
 
@@ -12,6 +12,7 @@ const { drinkId } = toRefs(props);
 const reviewText = ref("")
 const reviewRating = ref(2)
 const errorMessage = ref(null)
+const allReviews = ref([])
 
 async function uploadReview() {
     const { error } = await supabase
@@ -34,6 +35,21 @@ async function uploadReview() {
     }
     errorMessage.value = null
 }
+
+supabase
+    .from('drink_reviews')
+    .select('created_at, score, message')
+    .eq('drink_id', drinkId.value)
+    .order('created_at')
+    .limit(10)
+    .then((res) => {
+        const data = res.data
+        if (data === null) {
+            allReviews.value = []
+            return
+        }
+        allReviews.value = data
+    })
 </script>
 
 <template>
@@ -42,12 +58,22 @@ async function uploadReview() {
     </div>
     <div class="flex justify-center w-full">
         <div class="flex flex-col justify-center w-3/4">
-            <input class="m-2 p-2 bg-amber-100" type="text" :value="reviewText" />
-            <input class="m-2 p-2 bg-amber-100" type="number" max="5" min="0" :value="reviewRating" />
+            <textarea class="m-2 p-2 bg-amber-100" type="text" v-model="reviewText"></textarea>
+            <input class="m-2 p-2 bg-amber-100" type="number" max="5" min="0" v-model.number="reviewRating" />
             <button class="button" @click="uploadReview">Add review</button>
         </div>
     </div>
-    <div>
-        {{ drinkId }}
+    <div class="flex flex-col justify-center">
+        <div v-for="review in allReviews" key="review" class="flex justify-center">
+            <div class="flex flex-col rounded-xl bg-gray-200 w-3/4 p-4 m-2">
+                <span>
+                    {{ review.message }}
+                </span>
+                <div class="flex justify-between">
+                    <div>Score: {{ review.score }}</div>
+                    <div class="text-slate-600">{{ review.created_at }}</div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
