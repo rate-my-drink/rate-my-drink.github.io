25ce5b9fb3731473d292b0b03a7d4e53b77ee6ba
wagenrace <nijhof.dns@gmail.com>
2023-11-02 10:11:05 +0100

    make test work

--- src/views/Home.test.js
+++ src/views/Home.test.js
@@ -1,27 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { test } from 'vitest'
-import { mount } from '@vue/test-utils'
-import Home from '@/views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import { test } from "vitest";
 
-test('calculates totalNumPages correctly', () => {
+import { mount } from "@vue/test-utils";
+import Home from "@/views/Home.vue";
+
+test("calculates totalNumPages correctly", async () => {
   const router = createRouter({
     history: createWebHistory(),
     routes: [], // provide your routes here
-  })
+  });
 
   const wrapper = mount(Home, {
-    data() {
-      return {
-        totalNumDrinks: 100,
-        numbPerPage: 10
-      }
-    },
     global: {
-      plugins: [router]
-    }
-  })
-  
-  expect(wrapper.vm.totalNumDrinks).toBe(100)
-  expect(wrapper.vm.numbPerPage).toBe(10)
-  expect(wrapper.vm.totalNumPages).toBe(9)
-})
+      plugins: [router],
+    },
+  });
+
+  wrapper.vm.totalNumDrinks = 100
+  wrapper.vm.numbPerPage = 10
+
+  expect(wrapper.vm.totalNumPages).toBe(9);
+});
