50191415548b33f6511cf0f9c80e6769ef71d772
wagenrace <nijhof.dns@gmail.com>
2024-01-10 22:23:39 +0100

    Base url to .

--- vite.config.js
+++ vite.config.js
@@ -14,12 +14,12 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "/",
+  base: ".",
   test: {
     // enable jest-like global test APIs
     globals: true,
     // simulate DOM with happy-dom
     // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom'
-  }
+    environment: "happy-dom",
+  },
 });
