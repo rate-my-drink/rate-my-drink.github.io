c3dbb2d0c353b105e5c237a3284d71e90714d2f9
wagenrace <nijhof.dns@gmail.com>
2024-03-31 22:41:51 +0200

    add VitePWA

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
       reactivityTransform: true,
     }),
+    VitePWA({ registerType: "autoUpdate" }),
   ],
   resolve: {
     alias: {
