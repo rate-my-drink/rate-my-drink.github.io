7d58653640c6d17ddd50f96168e02729fe25a319
Tom Nijhof <nijhof.dns@gmail.com>
2024-03-31 23:06:08 +0200

    Tom/pwa (#40)

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
       reactivityTransform: true,
+    }),
+    VitePWA({
+      includeAssets: ["logo.svg", "apple-touch-icon.png"],
+      manifest: {
+        name: "Caffeine Critics",
+        short_name: "Caffeine Critics",
+        description: "Caffeine Critics: Rate teas and coffees you love or hate",
+        theme_color: "#f59e0b",
+        icons: [
+          {
+            src: "logo_192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo_512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
   resolve: {
